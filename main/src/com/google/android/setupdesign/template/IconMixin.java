2eb34e37de612f57f2609c8ab78e8e4e929bf313
Setup Wizard Team <android-setup-team-eng@google.com>
2019-05-14 15:00:32 +0800

    Import updated Android Setupdesign Library 248082482
    Copied from google3/third_party/java_src/android_libs/setupdesign
    
    Bug: 132366388
    Bug: 132366547
    Test: mm
    
    Included changes:
      - 248082482 Fix icon and header in Pattern security screen doesn't ap...
    
    PiperOrigin-RevId: 248082482
    Change-Id: I5fd6065ca6750423b134cb61ebf913ce2f444220

--- main/src/com/google/android/setupdesign/template/IconMixin.java
+++ main/src/com/google/android/setupdesign/template/IconMixin.java
@@ -24,6 +24,7 @@ import android.os.Build.VERSION;
 import android.os.Build.VERSION_CODES;
 import androidx.annotation.ColorInt;
 import androidx.annotation.DrawableRes;
+import androidx.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
@@ -83,19 +84,31 @@ public class IconMixin implements Mixin {
     }
 
     a.recycle();
-
-    ImageView iconImage = layout.findManagedViewById(R.id.sud_layout_icon);
-    if (iconImage != null
-        && (layout instanceof GlifLayout)
-        && ((GlifLayout) layout).shouldApplyPartnerHeavyThemeResource()) {
-      applyPartnerCustomizationStyle(context, iconImage);
-    }
   }
 
-  public void applyPartnerCustomizationStyle(Context context, ImageView iconImage) {
-    int gravity = PartnerStyleHelper.getLayoutGravity(context);
-    if (gravity != 0) {
-      setGravity(iconImage, gravity);
+  /** See {@link #applyPartnerCustomizationStyle(Context, ImageView)}. */
+  public void applyPartnerCustomizationStyle() {
+    final Context context = templateLayout.getContext();
+    final ImageView iconImage = templateLayout.findManagedViewById(R.id.sud_layout_icon);
+    applyPartnerCustomizationStyle(context, iconImage);
+  }
+
+  /**
+   * Use the given {@code iconImage} to apply heavy theme. If {@link
+   * com.google.android.setupdesign.GlifLayout#shouldApplyPartnerHeavyThemeResource()} is true,
+   * {@code iconImage} can be customized style from partner configuration.
+   *
+   * @param context The context of client activity.
+   * @param iconImage The icon image to use for apply heavy theme.
+   */
+  private void applyPartnerCustomizationStyle(Context context, @Nullable ImageView iconImage) {
+    if (iconImage != null
+        && (templateLayout instanceof GlifLayout)
+        && ((GlifLayout) templateLayout).shouldApplyPartnerHeavyThemeResource()) {
+      int gravity = PartnerStyleHelper.getLayoutGravity(context);
+      if (gravity != 0) {
+        setGravity(iconImage, gravity);
+      }
     }
   }
 
